d8f705df4270cfa8003b80024e2b49e2eb42a754
junzeLi <junze.li206@gmail.com>
2021-08-20 23:39:44 +1000

    edit header file

--- CI/application/views/header.php
+++ CI/application/views/header.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-      <title>Anyway header content</title>
+      <title>Anyway</title>
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
       <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
