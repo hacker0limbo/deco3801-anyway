739ea1075bc4b26372e67e9430f2923ebd7f42ab
junzeLi <junze.li206@gmail.com>
2021-08-20 15:21:20 +1000

    add header/footer

---
+++ CI/application/views/footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <p> footer content </p>
+            <p>&copy; 2021-<?php echo date("Y"); ?></p>
+        </footer>
+    </body>
+</html>
---
+++ CI/application/views/header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+      <title>Anyway header content</title>
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+      <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+    </head>
+    <body>
+        <p> header body </p>
