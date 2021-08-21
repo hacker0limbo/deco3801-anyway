6e20e55454842c3e9d021a11de1ac0a2c1671217
junzeLi <junze.li206@gmail.com>
2021-08-21 15:00:59 +1000

    set functions in login and signup

--- CI/application/controllers/Login.php
+++ CI/application/controllers/Login.php
@@ -2,12 +2,32 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
 	// index page
-	public function index()
-	{
+	// if already logged in, load homepage
+	// if remembered, load homepage
+	// set relevant values in session/cookies
+	public function index() {
 		$this->load->view('header');
 		$this->load->view('logIn');
 		$this->load->view('footer');
 	}
+
+
+	// submit login form and validate user information
+	// set relecant values in session/cookies
+	// fail -> show error message
+	// success -> show homepage 
+	public function validate_login() {
+
+	}
+
+
+	// log user out
+	// clear session and cookies
+	public function logout() {
+
+	}
+
+
+
 }
--- CI/application/controllers/Signup.php
+++ CI/application/controllers/Signup.php
@@ -4,10 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Signup extends CI_Controller {
 
 	// index page
-	public function index()
-	{
+	// load sign up form
+	public function index() {
 		$this->load->view('header');
 		$this->load->view('signUp');
 		$this->load->view('footer');
 	}
+
+
+	// sign up new account
+	// validate form information (unique username, valid email, password strength)
+	// send email validation
+	public function validate_signup() {
+
+	}
+
+
+
 }
