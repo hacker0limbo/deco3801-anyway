947bbd873ca62cfec0c59a46764db615ab10e689
Stephen <stephen.yin@outlook.com>
2021-08-31 23:58:36 +0800

    style footer and navbar

--- CI/application/views/footer.php
+++ CI/application/views/footer.php
@@ -1,5 +1,7 @@
-        <footer>
-            <p class="d-xl-flex justify-content-center">&copy; 2021 Copyright: DECO3801 Anyway</p>
+        <footer class="footer mt-auto py-3">
+            <div class="container">
+                <p class="text-center text-muted mb-0">&copy; 2021 Copyright: DECO3801 Anyway</p>
+            </div>
         </footer>
     </body>
 </html>
--- CI/application/views/header.php
+++ CI/application/views/header.php
@@ -3,41 +3,72 @@
     <title>Anyway - Your Perfect Medical Help In Australia</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
   </head>
     
-  <body>
+  <body class="h-100 d-flex flex-column">
     <!-- Navigation -->
-    <nav class="navbar-expand-md d-xl-flex align-items-xl-center justify-content-between" id="nav-bar">
-      <img class="d-xl-flex align-items-xl-end" id="logo">
-      <h1 class="d-xl-flex align-items-xl-end" id="website-name"> Anyway</h1>
-      
-      <!-- navigation bar links -->
-      <a class="nav-link d-xl-flex align-items-xl-end" href="<?php echo base_url(); ?>">Homepage</a>
-      <a class="nav-link d-xl-flex align-items-xl-end" href="<?php echo base_url(); ?>booking">Online Booking</a>
-      <a class="nav-link d-xl-flex align-items-xl-end" href="<?php echo base_url(); ?>information">Medical Information</a>
-      <a class="nav-link d-xl-flex align-items-xl-end" href="<?php echo base_url(); ?>checker">Symptom Checker</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <!-- Logo(Homepage) & toggle button for responsible size -->
+      <a class="navbar-brand" href="<?php echo base_url(); ?>">
+        <img class="logo" width="30" height="30" src="<?php echo base_url(); ?>assets/img/logo.png"/>
+        <span>Anyway</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <!-- Search bar -->
-      <input type="search" placeholder="Search" name="search" aria-label="Search">
+      <!-- collapsed content for responsible size -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>booking">Online Booking</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>information">Medical Information</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>checker">Symptom Checker</a>
+          </li>
+        </ul>
 
-      <!-- 做一个if 判断登陆没有-->
-      <!-- logout 要在login的controller里加function-->
-      <?php if(!$this->session->userdata('logged_in')): ?>
-        <a class="d-xl-flex align-items-xl-end"  href="<?php echo base_url(); ?>login">LogIn</a>
-      <?php else: ?>
-        <a class="d-xl-flex align-items-xl-end"  href="<?php echo base_url(); ?>login/logout">LogOut</a>
-      <?php endif; ?>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+        </form>
 
-      <!-- 
-      <a class="d-xl-flex align-items-xl-end"  href="<?php echo base_url(); ?>login/logout">LogOut</a>
-      <a class="d-xl-flex align-items-xl-end"  href="<?php echo base_url(); ?>signup">SignUp</a>
-      <a class="d-xl-flex align-items-xl-end"  href="<?php echo base_url(); ?>profile">My Profile</a>
-      -->
+        <!-- 做一个if 判断登陆没有-->
+        <!-- logout 要在login的controller里加function-->
+        <ul class="navbar-nav">
+          <?php if(!$this->session->userdata('logged_in')): ?>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>login">
+                <i class="bi bi-person-plus"></i>
+                Login
+              </a>
+            </li>
+          <?php else: ?>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>login/logout">
+                <i class="bi bi-person-x"></i>
+                Logout
+              </a>
+            </li>
+          <?php endif; ?>
 
-      <!-- button 翻译整个页面 -->
-      <a class="d-xl-flex align-items-xl-end"  href="">Languages</a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Languages
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <span class="dropdown-item" href="#">English</span>
+              <span class="dropdown-item" href="#">简体中文</span>
+            </div>
+          </li>
+        </ul>
+      </div>
 
     </nav>
 
