fb02decad176adbf0288ba4873ddc4320f99ad0b
Stephen <stephen.yin@outlook.com>
2021-09-06 01:44:42 +0800

    add i18n, delete unused files

--- CI/application/views/header.php
+++ CI/application/views/header.php
@@ -1,89 +1,97 @@
 <html>
   <head>
     <title>Anyway - Your Perfect Medical Help In Australia</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js" defer></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js" defer></script>
+
+    <script type="module" src="<?php echo base_url(); ?>assets/js/index.js"></script>
+
   </head>
     
   <body class="h-100 d-flex flex-column">
+    <div id="app">
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <!-- Logo(Homepage) & toggle button for responsible size -->
-      <a class="navbar-brand" href="<?php echo base_url(); ?>">
-        <img class="logo" width="30" height="30" src="<?php echo base_url(); ?>assets/img/logo.png"/>
-        <span>Anyway</span>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Logo(Homepage) & toggle button for responsible size -->
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">
+          <img class="logo" width="30" height="30" src="<?php echo base_url(); ?>assets/img/logo.png"/>
+          <span>Anyway</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <!-- collapsed content for responsible size -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url(); ?>booking">Online Booking</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url(); ?>information">Medical Information</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url(); ?>checker">Symptom Checker</a>
-          </li>
-        </ul>
-
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-        </form>
-
-        <!-- 做一个if 判断登陆没有-->
-        <!-- logout 要在login的controller里加function-->
-        <ul class="navbar-nav">
-          <!-- not login, only show login button -->
-          <?php if(!$this->session->userdata('logged_in')): ?>
+        <!-- collapsed content for responsible size -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="nav-link" href="<?php echo base_url(); ?>login">
-                <i class="bi bi-person-plus"></i>
-                Login
-              </a>
+              <a class="nav-link" href="<?php echo base_url(); ?>booking">{{ $t("header.onlineBooking") }}</a>
             </li>
-          <?php else: ?>
-            <!-- user logged in, show dropdown list with profile and logout button -->
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>information">{{ $t("header.medicalInformation") }}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>checker">{{ $t("header.symptomChecker") }}</a>
+            </li>
+          </ul>
+
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">{{ $t("header.search") }}</button>
+          </form>
+
+          <!-- 做一个if 判断登陆没有-->
+          <!-- logout 要在login的controller里加function-->
+          <ul class="navbar-nav">
+            <!-- not login, only show login button -->
+            <?php if(!$this->session->userdata('logged_in')): ?>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>login">
+                  <i class="bi bi-person-plus"></i>
+                  {{ $t("header.login") }}
+                </a>
+              </li>
+            <?php else: ?>
+              <!-- user logged in, show dropdown list with profile and logout button -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="authDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="bi bi-person-check"></i>
+                  <?php echo $this->session->userdata("username"); ?>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="authDropdown">
+                  <a class="dropdown-item" href="<?php echo base_url(); ?>profile">
+                    <i class="bi bi-person"></i>
+                    Profile
+                  </a>
+                  <a class="dropdown-item" href="<?php echo base_url(); ?>login/logout">
+                    <i class="bi bi-person-x"></i>
+                    Logout
+                  </a>
+                </div>
+              </li>
+            <?php endif; ?>
+
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="authDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="bi bi-person-check"></i>
-                <?php echo $this->session->userdata("username"); ?>
+              <a class="nav-link dropdown-toggle" href="#" id="languagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ $t("header.languages") }}
               </a>
-              <div class="dropdown-menu" aria-labelledby="authDropdown">
-                <a class="dropdown-item" href="<?php echo base_url(); ?>profile">
-                  <i class="bi bi-person"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="<?php echo base_url(); ?>login/logout">
-                  <i class="bi bi-person-x"></i>
-                  Logout
-                </a>
+              <div class="dropdown-menu" aria-labelledby="languagesDropdown">
+                <span role="button" @click="() => changeLocale('en')" class="dropdown-item" :class="{ active: $i18n.locale === 'en' }">
+                  English
+                </span>
+                <span role="button" @click="() => changeLocale('ch')" class="dropdown-item" :class="{ active: $i18n.locale === 'ch' }">
+                  简体中文
+                </span>
               </div>
             </li>
-          <?php endif; ?>
+          </ul>
+        </div>
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="languagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Languages
-            </a>
-            <div class="dropdown-menu" aria-labelledby="languagesDropdown">
-              <a class="dropdown-item" href="#">English</a>
-              <a class="dropdown-item" href="#">简体中文</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-
-    </nav>
-
+      </nav>
+    </div>
     
 
 
---
+++ CI/assets/js/i18n.js
@@ -0,0 +1,23 @@
+// i18n language config
+export const messages = {
+	en: {
+		header: {
+			medicalInformation: "Medical Information",
+			onlineBooking: "Online Booking",
+			symptomChecker: "Symptom Checker",
+			search: "Search",
+			login: "Login",
+			languages: "Languages",
+		},
+	},
+	ch: {
+		header: {
+			medicalInformation: "医疗信息",
+			onlineBooking: "在线预约",
+			symptomChecker: "症状检查",
+			search: "搜索",
+			login: "登录",
+			languages: "语言设置",
+		},
+	},
+};
