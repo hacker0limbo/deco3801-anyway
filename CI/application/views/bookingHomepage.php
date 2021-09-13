5298b31481d21a08cd270e71584701b0529c7fb0
Kexu <douimp@qq.com>
2021-09-13 18:18:38 +1000

    [feat] create static web page 1. build view for booking feature

--- CI/application/views/bookingHomepage.php
+++ CI/application/views/bookingHomepage.php
@@ -1,1 +1,103 @@
-<p> booking homepage </p> 
+
+<div class="h-100">
+
+    <div id="input_area" class="col-md-10 offset-md-1 mt-5 d-flex flex-row flex-wrap justify-content-center">
+        
+        <div class="form-group col-md-4 d-flex align-items-center">
+            <img class="img-fluid img-thumbnail mr-3" style="width: 50px; height: 50px;" src="assets/img/logo.png"/>
+            <input type="text" class="form-control" name="provider" placeholder="Specialty or provider name">
+            <?php echo form_error('username'); ?>
+        </div>
+
+        <div class="form-group col-md-4 d-flex align-items-center">
+            <img class="img-fluid img-thumbnail mr-3" style="width: 50px; height: 50px;" src="assets/img/logo.png"/>
+            <input type="text" class="form-control" name="suburb" placeholder="Suburb or postcode">
+            <?php echo form_error('username'); ?>
+        </div>
+
+        <div class="form-group col-md-4 d-flex align-items-center">
+            <img class="img-fluid img-thumbnail mr-3" style="width: 50px; height: 50px;" src="assets/img/logo.png"/>
+            <input type="text" class="form-control" name="sex" placeholder="Doctor's sex">
+            <?php echo form_error('username'); ?>
+        </div>
+
+        <div class="form-group col-md-3 d-flex align-items-center">
+            <img class="img-fluid img-thumbnail mr-3" style="width: 50px; height: 50px;" src="assets/img/logo.png"/>
+            <input type="text" class="form-control" name="date" placeholder="10/04/2000">
+            <?php echo form_error('username'); ?>
+        </div>
+
+
+        <div class="form-group col-md-3 d-flex align-items-center">
+            <img class="img-fluid img-thumbnail mr-3" style="width: 50px; height: 50px;" src="assets/img/logo.png"/>
+            <input type="text" class="form-control" name="date" placeholder="10/04/2000">
+            <?php echo form_error('username'); ?>
+        </div>
+
+        <div class="form-group col-md-3 d-flex align-items-center">
+            <img class="img-fluid img-thumbnail mr-3" style="width: 50px; height: 50px;" src="assets/img/logo.png"/>
+            <a role="button" class="btn btn-outline-dark btn-block">Search</a>
+        </div>
+
+    </div>
+
+    <div class="col-md-10 offset-md-1 mt-2 h3"> Avaliable Results: </div>
+
+    <div id="filter_result" class="col-md-10 offset-md-1 mt-4 h-50">
+
+        <!-- result item card -->
+        <div class="card card-body mb-3">
+            <div class="media align-items-center text-center text-lg-left">
+                <div class="mr-2 mb-3 mb-lg-0"> <img src="assets/img/logo.png" width="150" height="150" alt=""> </div>
+                <div class="media-body">
+                    <h6 class="media-title font-weight-semibold"> <a href="?">Queen Elizabeth II Jubilee Hospital</a> </h6>
+                    <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
+                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true"># vaccine</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true"># queensland</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true"># 8:00 - 16:00</a></li>
+                    </ul>
+                    <p class="mb-3">The Queen Elizabeth II Jubilee Hospital is a medium metropolitan adult facility providing a range of inpatient and outpatient services and a 24-hour emergency department. Our endoscopy unit is Australia’s most advanced diagnostic facility and has the capacity to see more than 5000 patients a year. </p>
+                    <ul class="list-inline list-inline-dotted mb-0">
+                        <li class="list-inline-item"><a href="#">Find out more</a></li>
+                    </ul>
+                </div>
+                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
+                    <h3 class="mb-0 font-weight-semibold">23.1 Km</h3>
+                    <div> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
+                    <div class="text-muted">Today full booked</div> 
+                    <button type="button" class="btn btn-primary mt-4 text-white">Book Now</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- result item card -->
+        <div class="card card-body mb-3">
+            <div class="media align-items-center text-center text-lg-left">
+                <div class="mr-2 mb-3 mb-lg-0"> <img src="assets/img/logo.png" width="150" height="150" alt=""> </div>
+                <div class="media-body">
+                    <h6 class="media-title font-weight-semibold"> <a href="?">Queen Elizabeth II Jubilee Hospital</a> </h6>
+                    <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
+                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true"># vaccine</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true"># queensland</a></li>
+                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true"># 8:00 - 16:00</a></li>
+                    </ul>
+                    <p class="mb-3">The Queen Elizabeth II Jubilee Hospital is a medium metropolitan adult facility providing a range of inpatient and outpatient services and a 24-hour emergency department. Our endoscopy unit is Australia’s most advanced diagnostic facility and has the capacity to see more than 5000 patients a year. </p>
+                    <ul class="list-inline list-inline-dotted mb-0">
+                        <li class="list-inline-item"><a href="#">Find out more</a></li>
+                    </ul>
+                </div>
+                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
+                    <h3 class="mb-0 font-weight-semibold">23.1 Km</h3>
+                    <div> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
+                    <div class="text-muted">Today full booked</div> 
+                    <button type="button" class="btn btn-primary mt-4 text-white">Book Now</button>
+                </div>
+            </div>
+        </div>
+
+
+        
+    </div>
+    
+
+</div>
