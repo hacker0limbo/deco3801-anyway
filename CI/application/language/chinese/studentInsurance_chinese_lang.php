c3ee9318f25e7768716b8ac0db528e67738503ab
junzeLi <junze.li206@gmail.com>
2021-10-01 02:08:02 +1000

    add language file and content to student insurance

---
+++ CI/application/language/chinese/studentInsurance_chinese_lang.php
@@ -0,0 +1,41 @@
+<?php
+    $lang['medicalService']='医疗服务';
+    $lang['medicalInsurance'] = '医疗保险';
+    $lang['onlineBooking'] = '寻找医生';
+    $lang['symptomChecker'] = '症状检查';
+    $lang['serviceType'] = '医疗服务类型';
+    $lang['medicalProcess'] = '医疗服务流程';
+    $lang['search'] = '搜索';
+    $lang['login'] = '登录';
+    $lang['languages'] = '语言设置';
+    $lang['websiteName'] = '安游';
+    $lang['websiteTitle'] = '安游 - 您在澳洲的最佳医疗助手';
+
+
+    $lang['students'] = '海外学生';
+    $lang['visitors'] = '来澳旅客';
+    $lang['citizens'] = '公民';
+
+    $lang['OSHC'] = '海外学生健康保险（OSHC）';
+    $lang['OSHC_des'] = '如果您是持有临时学生签证的海外学生，在您在澳大利亚学习期间，您需要保证您持有足够时长的海外学生健康保险以获取您的签证。 这意味着您需要购买海外学生健康保险 (OSHC) 并在您在澳大利亚并持有学生签证期间及时更新您的保单。
+    OSHC 协助国际学生支付他们在澳大利亚期间可能需要的医疗和住院护理费用。 OSHC 还包括救护车保险和部分的药品。';
+
+    $lang['coverage'] = 'OSHC 涵盖哪些内容？';
+    $lang['coverage_des'] = '海外学生保险的政策包括帮助您支付住院和医疗费用，以及救护车服务。如需更多信息，请咨询您的 OSHC 保险公司。
+    海外学生保险不支付全科治疗项目（辅助或额外保险），例如牙科、光学或物理治疗。 如果您需要您的保险报销这些治疗项目，您可以购买由OSHC提供者提供的 Extras OSHC 或任何澳大利亚私人健康保险公司的全科治疗保险。 您也可以选择为您的海外学生保险补充其他保险，例如国际旅行保险。';
+
+    $lang['covid'] = '海外学生保险和 COVID-19';
+    $lang['covid_des'] = '海外学生保险包含了冠状病毒 (COVID-19) 相关疾病的保险，主要是“肺和胸部”、“肾脏和膀胱”和“慢性肾功能衰竭透析”临床类别。
+    如果您是由于澳大利亚政府为应对全球大流行而实施的强制旅行限制而滞留在澳大利亚的国际学生，我们建议您联系您的私人医疗保险公司寻求进一步帮助，以确保在此期间承保的连续性。
+    我们建议持有学生签证的因冠状病毒 (COVID-19) 而在海外的国际学生联系他们的私人医疗保险公司，了解在此期间可用的选择。';
+    
+    $lang['purchase'] = '我可以去哪里购买海外学生保险？';
+    $lang['purchase_des'] = '海外学生保险由部分保险公司根据与卫生部签订的协议提供，以合理的费用为学生提供足够的健康保险。只有少数注册医疗保险公司有资格提供海外学生保险：    ';
+    $lang['purchase_i1'] = 'ahm OSHC: www.ahmoshc.com';
+    $lang['purchase_i2'] = 'Allianz Care Australia (Peoplecare): www.allianzcare.com.au/en/student-visa-oshc.html';
+    $lang['purchase_i3'] = 'BUPA Australia: www.bupa.com.au/health-insurance/oshc';
+    $lang['purchase_i4'] = 'CBHS International Health: www.cbhsinternationalhealth.com.au/overseas-students-oshc';
+    $lang['purchase_i5'] = 'Medibank Private: www.medibank.com.au';
+    $lang['purchase_i6'] = 'NIB OSHC: www.nib.com.au';
+
+    ?>
