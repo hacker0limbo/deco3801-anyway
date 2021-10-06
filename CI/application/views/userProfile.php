690cfd28ee3e1b3337b697539a98d7bb77b417b7
junzeLi <junze.li206@gmail.com>
2021-10-06 21:22:04 +1000

    modify profile

--- CI/application/views/userProfile.php
+++ CI/application/views/userProfile.php
@@ -10,7 +10,7 @@
 <div class="container">
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>service"><?php echo $homepage?></a></li>
+      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>"><?php echo $homepage?></a></li>
       <li class="breadcrumb-item active" aria-current="page"><?php echo $userProfile?></li>
     </ol>
   </nav>
