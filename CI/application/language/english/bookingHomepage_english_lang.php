080874f9bdeb72dde7fc43d8eeafab02359a029c
junzeLi <junze.li206@gmail.com>
2021-10-14 15:50:12 +1000

    add language files and style to doctor page

---
+++ CI/application/language/english/bookingHomepage_english_lang.php
@@ -0,0 +1,21 @@
+<?php
+    $lang['filterInstruction'] = 'Apply your filters to find a suitable service:';
+    $lang['serviceType'] = 'Service Type';
+    $lang['distance'] = 'Distance';
+    $lang['availableNow'] = 'Available Now';
+    $lang['availableLater'] = 'Available Later';
+    $lang['openingHours'] = 'Opening Hours';
+    $lang['languages'] = 'Languages';
+    $lang['search'] = 'Search';
+    $lang['panelInstruction']='Find a suitable doctor by using the filter above, you may also navigate around the map to find the medical services.';
+    $lang['more']='More';
+    $lang['address'] = 'Address';
+    $lang['contact'] = 'Contact';
+    $lang['description'] = 'Description';
+    $lang['tags'] = 'Tags';
+    $lang['currentlyUnavailable'] = 'Currently Unavailable';
+    $lang['booking'] = 'Booking';
+    $lang['nextAvailable'] = 'Next Available';
+    $lang['billing'] = 'Billing';
+    $lang['rating'] = 'Rating';
+?>
