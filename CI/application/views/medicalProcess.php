d29675493f50c24fe60fc471f466d62203ba59a9
junzeLi <junze.li206@gmail.com>
2021-10-26 16:18:13 +1000

    add reference

--- CI/application/views/medicalProcess.php
+++ CI/application/views/medicalProcess.php
@@ -52,6 +52,42 @@
         </ul>
       </div>
 
+      <div style="margin-top: 4em; margin-bottom: 5em;" >
+        <h3 id="nonEmergency">Reference</h3>
+        <hr class="my-3" style="margin: auto;">
+        <p>
+          A Doctor’s Guide to a Good Appointment. (2021). Retrieved 26 October 2021, from https://www.nytimes.com/guides/well/make-the-most-of-your-doctor-appointment
+        </p>
+
+        <p>
+          Australia, A. (2021). 海外学生医疗保险 (OSHC). Retrieved 26 October 2021, from https://www.allianzcare.com.au/zh/student-visa-oshc.html
+        </p>
+
+        <p>
+          Difference between Hospital and Clinic | Hospital vs Clinic. (2021). Retrieved 26 October 2021, from http://www.differencebetween.info/difference-between-hospital-and-clinic
+        </p>
+
+        <p>
+          How to Get Medical Treatment in Australia - Health Guide. (2021). Retrieved 26 October 2021, from https://www.australia-backpackersguide.com/medical-treatment-australia/
+        </p>
+
+        <p>
+          Migration – 信为移民咨询. (2021). Retrieved 26 October 2021, from https://swmigration.com/cn/migration/
+        </p>
+
+        <p>
+          Oscar | Smart, simple health insurance. (2021). Retrieved 26 October 2021, from https://www.hioscar.com/faq/pcp-vs-specialist
+        </p>
+
+        <p>
+          The Australian Health System. (2021). Retrieved 26 October 2021, from https://www.health.gov.au/about-us/the-australian-health-system
+        </p>
+
+        <p>
+          澳大利亚的全科医学服务体系简介及启示. (2021). Retrieved 26 October 2021, from http://rs.yiigle.com/CN114798201412/598815.htm
+        </p>
+      </div>
+
 
     </div>
 
--- CI/application/views/visitorInsurance.php
+++ CI/application/views/visitorInsurance.php
@@ -1,7 +1,7 @@
 <div class="container" style="margin:0 !important; padding-left:0;">
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>service"><?php echo $medicalInsurance?> </a></li>
+      <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>insurance"><?php echo $medicalInsurance?> </a></li>
       <li class="breadcrumb-item active" aria-current="page"><?php echo $visitors.' '; echo $medicalInsurance?> </li>
     </ol>
   </nav>
@@ -45,6 +45,44 @@
         <p class="text-justify"><?php echo $purchase_p1 ?></p>
         <p class="text-justify"><?php echo $purchase_p2 ?></p>
       </div>
+
+
+      <div style="margin-top: 4em; margin-bottom: 5em;" >
+        <h3 id="nonEmergency">Reference</h3>
+        <hr class="my-3" style="margin: auto;">
+        <p>
+          A Doctor’s Guide to a Good Appointment. (2021). Retrieved 26 October 2021, from https://www.nytimes.com/guides/well/make-the-most-of-your-doctor-appointment
+        </p>
+
+        <p>
+          Australia, A. (2021). 海外学生医疗保险 (OSHC). Retrieved 26 October 2021, from https://www.allianzcare.com.au/zh/student-visa-oshc.html
+        </p>
+
+        <p>
+          Difference between Hospital and Clinic | Hospital vs Clinic. (2021). Retrieved 26 October 2021, from http://www.differencebetween.info/difference-between-hospital-and-clinic
+        </p>
+
+        <p>
+          How to Get Medical Treatment in Australia - Health Guide. (2021). Retrieved 26 October 2021, from https://www.australia-backpackersguide.com/medical-treatment-australia/
+        </p>
+
+        <p>
+          Migration – 信为移民咨询. (2021). Retrieved 26 October 2021, from https://swmigration.com/cn/migration/
+        </p>
+
+        <p>
+          Oscar | Smart, simple health insurance. (2021). Retrieved 26 October 2021, from https://www.hioscar.com/faq/pcp-vs-specialist
+        </p>
+
+        <p>
+          The Australian Health System. (2021). Retrieved 26 October 2021, from https://www.health.gov.au/about-us/the-australian-health-system
+        </p>
+
+        <p>
+          澳大利亚的全科医学服务体系简介及启示. (2021). Retrieved 26 October 2021, from http://rs.yiigle.com/CN114798201412/598815.htm
+        </p>
+      </div>
+
 
     </div>
 </div>
